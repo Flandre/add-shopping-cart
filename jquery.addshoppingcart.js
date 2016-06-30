43c9fc6623da52d96eb321c6f84cc0c2dabf993c
Flandre <flantan233@gmail.com>
2016-06-30 12:30:17 +0800

    生成插件

--- jquery.addshoppingcart.js
+++ jquery.addshoppingcart.js
@@ -1,20 +1,24 @@
+$.fn.addShoppingcart = function(obj){
+  this.on('click', function(){
+    var btnTop = this.offsetTop - $('body')[0].scrollTop,
+      btnLeft = this.offsetLeft,
+      mallbarTop = obj[0].offsetTop,
+      mallbarLeft = obj[0].offsetLeft;
+
+
+    var timeout = 1000;
+
+    var frame = 100;
+    //  计算物体垂直方向初速度，按钮离顶部越近则初速度越小
+    var V0 = -btnTop / 40;
+    //  计算物体加速度
+    acceleration = ((mallbarTop - btnTop) - V0 * frame) * 2 / (frame * frame);
+
+    runAnimation($('.test'), btnTop, V0, btnLeft, (mallbarLeft - btnLeft) / frame, mallbarLeft, timeout / frame);
+  })
+};
+
 var acceleration;
-$('#btn').on('click', function () {
-  var btnTop = $('#btn')[0].offsetTop - $('body')[0].scrollTop,
-    btnLeft = $('#btn')[0].offsetLeft,
-    mallbarTop = $('#mallbar')[0].offsetTop,
-    mallbarLeft = $('#mallbar')[0].offsetLeft;
-
-  var timeout = 1000;
-
-  var frame = 100;
-  //  计算物体垂直方向初速度，按钮离顶部越近则初速度越小
-  var V0 = -btnTop / 40;
-  //  计算物体加速度
-  acceleration = ((mallbarTop - btnTop) - V0 * frame) * 2 / (frame * frame);
-
-  runAnimation($('.test'), btnTop, V0, btnLeft, (mallbarLeft - btnLeft) / frame, mallbarLeft, timeout / frame);
-});
 
 function runAnimation(goods, top, topStep, left, leftStep, maxRight, timeout) {
   if (left < maxRight) {
